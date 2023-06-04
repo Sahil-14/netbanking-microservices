9302ae0b206d27f495fe24b468cbc9bbd1a837f8
sahil shimpi <srshimpi14@gmail.com>
2023-06-04 22:30:34 +0530

    config service setup completed

---
+++ netbanking-config-server/src/test/java/com/srs/finance/netbankingconfigserver/NetbankingConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srs.finance.netbankingconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetbankingConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
